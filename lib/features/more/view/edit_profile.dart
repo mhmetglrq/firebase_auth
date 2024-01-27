c8f7678fd7e662995fe710ceba4293e3285bb779
Mehmet Güler <mhmetglr.q@gmail.com>
2024-01-27 22:23:45 +0300

    Video 18 Finished!

--- lib/features/more/view/edit_profile.dart
+++ lib/features/more/view/edit_profile.dart
@@ -1,11 +1,16 @@
+import 'dart:io';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_auth_app/common/repository/common_firebase_storage.dart';
 import 'package:flutter_auth_app/common/sizes.dart';
 import 'package:flutter_auth_app/features/more/controller/more_controller.dart';
 import 'package:flutter_auth_app/models/user_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../../../common/colors.dart';
+import '../../../common/utils.dart';
 
 class EditProfile extends StatefulWidget {
   const EditProfile({super.key, required this.currentUser});
@@ -26,6 +31,12 @@ class _EditProfileState extends State<EditProfile> {
   final TextEditingController _githubController = TextEditingController();
   final TextEditingController _linkedinController = TextEditingController();
   final TextEditingController _mediumController = TextEditingController();
+  XFile? _image;
+
+  void selectImage() async {
+    _image = await pickImageFromGallery();
+    setState(() {});
+  }
 
   @override
   void initState() {
@@ -69,11 +80,16 @@ class _EditProfileState extends State<EditProfile> {
             children: [
               Padding(
                 padding: vertical10,
-                child: CircleAvatar(
-                  backgroundColor: profilePhotoCircleColor,
-                  radius: 70,
-                  backgroundImage: CachedNetworkImageProvider(
-                    widget.currentUser.profilePhoto!,
+                child: GestureDetector(
+                  onTap: () => selectImage(),
+                  child: CircleAvatar(
+                    backgroundColor: profilePhotoCircleColor,
+                    radius: 70,
+                    backgroundImage: _image != null
+                        ? FileImage(File(_image!.path)) as ImageProvider
+                        : CachedNetworkImageProvider(
+                            widget.currentUser.profilePhoto!,
+                          ),
                   ),
                 ),
               ),
@@ -175,8 +191,18 @@ class _EditProfileState extends State<EditProfile> {
                         widget.currentUser.linkedin = _linkedinController.text;
                         widget.currentUser.medium = _mediumController.text;
 
+                        if (_image != null) {
+                          widget.currentUser.profilePhoto = await ref
+                              .read(commonFSRepositoryProvider)
+                              .storeFileToFirebase(
+                                "profilePhotos/${widget.currentUser.uid}",
+                                File(_image!.path),
+                              );
+                        } else {
+                          widget.currentUser.profilePhoto =
+                              widget.currentUser.profilePhoto;
+                        }
                         UserModel userModel = widget.currentUser;
-
                         await ref
                             .read(moreControllerProvider)
                             .updateProfile(userModel)
